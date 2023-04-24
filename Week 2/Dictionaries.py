f7018cea3bff88585ee284593843ae6e188095f3
arqam.mirza <mirzaarqambaig@gmail.com>
2023-04-24 23:11:53 +0500

    Folderized per weeks and added files

--- Week 2/Dictionaries.py
+++ Week 2/Dictionaries.py
@@ -13,9 +13,9 @@ print(Dict[(0, 1)])
 
 # Create a sample dictionary
 
-release_year_dict = {"Thriller": "1982", "Back in Black": "1980", \
-                    "The Dark Side of the Moon": "1973", "The Bodyguard": "1992", \
-                    "Bat Out of Hell": "1977", "Their Greatest Hits (1971-1975)": "1976", \
+release_year_dict = {"Thriller": "1982", "Back in Black": "1980",
+                    "The Dark Side of the Moon": "1973", "The Bodyguard": "1992",
+                    "Bat Out of Hell": "1977", "Their Greatest Hits (1971-1975)": "1976",
                     "Saturday Night Fever": "1977", "Rumours": "1977"}
 print(release_year_dict)
 
